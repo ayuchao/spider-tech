4e97f8c228b87c01d2a7ada52950b02e48cea2ca
ayuchao <877348180@qq.com>
2021-11-23 09:20:57 +0800

    v1

---
+++ main.py
@@ -0,0 +1,24 @@
+# 九、流程控制
+
+# if else, if  elif else使用
+
+money = (input('今天发了多少钱啊： ')
+# if money < 1000:
+#     print('去快乐一下吧。。。')
+# elif money < 1500:
+#     print('服务可以了吧？')
+# elif money < 2000:
+#     print('哈哈哈也不错啊。。。')
+# elif money < 5000:
+#     print('可以找外围了啊。。。')
+# elif money < 8000:
+#     print('大哥，带带我')
+# else:
+#     print('回家吃泡面吧。。。')
+
+
+for i in money:
+    if i > 1000:
+        print('1000')
+    else:
+        print(1)
